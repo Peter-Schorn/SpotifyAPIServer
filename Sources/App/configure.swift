c578965c26339eba58f2f32fad308ba2540dd920
Peter-Schorn <petervschorn@gmail.com>
2021-05-10 19:07:35 -0500

    local server now runs on port 7000 instead of 8080.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,11 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
 
+    if app.environment == .development {
+        // `RedirectListener` already uses 8080
+        app.http.server.configuration.port = 7000
+    }
+
     // register routes
     try routes(app)
 
