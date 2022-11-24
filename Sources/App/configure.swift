0e2fb29254f1aa4d33f4fe8c436645f4748b971d
Peter Schorn <petervschorn@gmail.com>
2022-11-24 08:19:36 -0600

    Updated Dockerfile.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,7 @@ public func configure(_ app: Application) throws {
 
     if [.development, .testing].contains(app.environment) {
         // `RedirectListener` already uses 8080
-        app.http.server.configuration.port = 7000
+       app.http.server.configuration.port = 7000
     }
 
     // register routes
--- docker_linux.sh
+++ docker_linux.sh
@@ -4,8 +4,11 @@ docker run \
     --rm \
     --volume "$(pwd):/src" \
     --workdir "/src" \
-    -it \
+    -i \
+    -t \
     --privileged \
+    -p 8080:8080 \
+    -p 7000:7000
     -e SPOTIFY_DATA_DUMP_FOLDER="/logs" \
     -e CLIENT_ID=$SPOTIFY_SWIFT_TESTING_CLIENT_ID \
     -e CLIENT_SECRET=$SPOTIFY_SWIFT_TESTING_CLIENT_SECRET \
