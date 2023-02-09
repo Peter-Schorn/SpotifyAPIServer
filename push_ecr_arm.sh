d6b74bf3285b1294d67e4d5a91a43de8fd4112e4
Peter Schorn <petervschorn@gmail.com>
2023-02-09 10:22:40 -0600

    formatting change in push_ecr.sh (test ssh)

--- push_ecr_arm.sh
+++ push_ecr_arm.sh
@@ -1,5 +1,5 @@
 aws ecr get-login-password --region us-east-1 | \
-docker login --username AWS --password-stdin 947734355387.dkr.ecr.us-east-1.amazonaws.com
+    docker login --username AWS --password-stdin 947734355387.dkr.ecr.us-east-1.amazonaws.com
 
 docker buildx build . \
     --platform linux/amd64,linux/arm64/v8 \
